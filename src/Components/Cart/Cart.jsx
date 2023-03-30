d30e96e402d17d25f8514f4a4067f192b15f506e
Asaduzzaman Ali <theasad.dev@gmail.com>
2023-03-30 23:19:30 +0600

    Cart Function Added

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan, faCreditCard } from '@fortawesome/free-solid-svg-icons'
 import './Cart.css'
+import Product from '../Product/Product';
 
 const Cart = ({ cart }) => {
     console.log(cart)
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     
-    for (const item of cart) {
-        total = total + item.price;
-        shipping = shipping + item.shipping;
+    for (const product of cart) {
+        // Instead this code We write code on handleAddToCart()
+        // if(product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+        // product.quantity = product.quantity || 1; // Shortcut
+        total = total + (product.price * product.quantity);
+        shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const totalWithoutTax = total + shipping;
     const tax = 5;
@@ -23,7 +31,7 @@ const Cart = ({ cart }) => {
         <div className='cart'>
             <h3 className='cart-title'>Order Summary</h3>
             <div className="cart-details">
-                <p><b>Selected Item:</b> {cart.length}</p>
+                <p><b>Selected Item:</b> {quantity}</p>
                 <p><b>Total Price:</b> ${total}</p>
                 <p><b>Total Shipping Charge:</b> ${shipping}</p>
                 <p><b>Tax:</b> ${taxAmount.toFixed(2)}</p>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -37,7 +37,20 @@ const Shop = () => {
     }, [products])
 
     const addToCartHandler = (product) => {
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        let newCart = [];
+        // if product does not exsist in the cart, set quantity 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id)
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists]
+        }
         setCart(newCart);
         addToDb(product.id)
     }
